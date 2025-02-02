75760f203c8d64481061ff8ed828946be41a853e
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-02-02 16:21:07 +0000

    Add lint, fmt, test configs

--- test/slim/lib_test.clj
+++ test/slim/lib_test.clj
@@ -1,3 +1,16 @@
 (ns slim.lib-test
   (:require [clojure.test :refer [deftest testing is]]
             [slim.lib :as lib]))
+
+(deftest get-version-test
+  (testing "get-version without snapshot"
+    (is (= "1.0.0"
+           (#'lib/get-version "1.0.0" false))))
+
+  (testing "get-version with snapshot"
+    (is (= "1.0.0-SNAPSHOT"
+           (#'lib/get-version "1.0.0" true))))
+
+  (testing "get-version with nil snapshot (defaults to false)"
+    (is (= "2.1.0"
+           (#'lib/get-version "2.1.0" nil)))))
